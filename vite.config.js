bc8a4e69970218443cf1585ef804d6f6d3b8df51
a.fifaliana <dev1.magento@dhk.be>
2025-06-23 13:00:37 +0300

    change vite config route config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: '/landing_page/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                product: resolve(__dirname, 'product.html'), // 👈 Add this
+            },
+        },
+    },
 });
